8a277208d10b12e9a47b516f6aeb049e6d2bd21e
robekoki <kokinsroberts@gmail.com>
2026-05-07 15:10:07 +0300

    transfer stuff from arduino ide

--- arduino/button_interrupt/interrupt_button/src/main.cpp
+++ arduino/button_interrupt/interrupt_button/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <stdint.h>
+#include "timer.h"
+#include "button.h"
 
-// put function declarations here:
-int myFunction(int, int);
+typedef struct {
+  uint8_t mask;
+  uint8_t state;
+  unsigned long last_blink_time;
+  unsigned long blink_interval;
+} LED;
+
+static LED led1 = { (1u << 5), 0, 0, 500ul };
+
+void led_init(LED *led) {
+  DDRB |= led->mask;
+  PORTB &= ~led->mask;
+  led->state = 0;
+  led->last_blink_time = millis();
+}
+
+void led_on(LED *led) {
+  PORTB |= led->mask;
+  led->state = 1;
+}
+
+void led_off(LED *led) {
+  PORTB &= ~led->mask;
+  led->state = 0;
+}
+
+void led_toggle(LED *led) {
+  if (led->state) {
+    led_off(led);
+  } else {
+    led_on(led);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  led_init(&led1);
+  Button_init();
+  timer1_init_1ms();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (Button_wasPressed()) {
+    led_toggle(&led1);
+  }
 }
